1e74f33055bb3d3cacdaa6747248ecb28096f48c
i515295 <e.leyboshor@sap.com>
2021-02-11 23:21:44 +0200

    add readme

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,8 +1,8 @@
 
 variable "aws_access_key" {
-  default = "AKIAZ6VTQG7B2I3A3EPF"
+  default = ""
 }
 variable "aws_secret_key" {
-  default = "HxA85PNSGTJO29YR8iHz4lTKgwmDXHVOvdY0/m+O"
+  default = ""
 }
 

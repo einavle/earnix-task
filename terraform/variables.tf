0d44194b285c8a106541ece403a1732520bde644
einavle <e.leyboshor@sap.com>
2021-02-12 01:01:45 +0200

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,8 +1,8 @@
 
 variable "aws_access_key" {
-  default = "AKIAZ6VTQG7B35MO4G4W"
+  default = ""
 }
 variable "aws_secret_key" {
-  default = "JkcRLuv0/FteD9HIRGpkI5CHcT58w2FlRB63kCZw"
+  default = ""
 }
 
